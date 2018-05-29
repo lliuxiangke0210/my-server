45e4d8ed54bf899cecf99e3c1ee5f36fbeddf8cc
liuxiangke0210 <liuxiangke0210@sina.com>
2018-05-29 09:33:10 +0800

    增加日志文件 logback.xml

--- src/main/java/com/zetyun/mml/controller/AgentInfoController.java
+++ src/main/java/com/zetyun/mml/controller/AgentInfoController.java
@@ -1,5 +1,7 @@
 package com.zetyun.mml.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,42 @@ import com.zetyun.mml.bean.CacheManager;
 @RequestMapping("/agent")
 public class AgentInfoController {
 
+	protected static Logger logger = LoggerFactory.getLogger(AgentInfoController.class);
+
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
 	public String sayWorld(@PathVariable("name") String name) {
+		logger.error("sayWorld自己定义的日志输出异常");
 		return "Hello " + name + " , are you ok? you are the best!";
+	}
+
+	@RequestMapping("/div")
+	public String helloworld() {
+		logger.error("sayWorld自己定义的日志输出异常");
+		try {
+			int num = 10;
+			int div = 0;
+			int result = num / div;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+
+			for (StackTraceElement errorinfo : e.getStackTrace()) {
+				logger.error(errorinfo.toString());
+			}
+			// 记录到日志
+			logger.error("GlobalExceptionHandler,捕获异常:" + e.getMessage());
+
+		}
+
+		return "Hello world!";
+	}
+
+	@RequestMapping("/hello/{name}")
+	public String helloName(@PathVariable String name) {
+		logger.error("sayWorld自己定义的日志输出异常");
+		logger.debug("访问helloName,Name={}", name);
+
+		return "Hello " + name;
 	}
 
 	/**
